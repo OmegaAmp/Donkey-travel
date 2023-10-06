68bd191bc9e35228bbc15c5c3f888af11c8dccc9
JoranBoonstoppel <boonstoppeljoran@gmail.com>
2023-10-06 14:34:18 +0200

    test

--- components/Boekitem.tsx
+++ components/Boekitem.tsx
@@ -1,26 +1,34 @@
+import React from 'react';
 
 
-type    blogtype = {
-    title: string
-}
+type BoekingenProps = {
+    id: number;
+    StartDatum: string;
+    PINCode: number;
+    FKtochtenID: number;
+    FKklantenID: number;
+    FKstatussenID: number;
+};
 
-export default function Boekitem({title} :blogtype) {
 
-    return(
+function BoekingenItem({
+                           id,
+                           StartDatum,
+                           PINCode,
+                           FKtochtenID,
+                           FKklantenID,
+                           FKstatussenID,
+                       }: BoekingenProps) {
+    return (
         <section>
-            <h1>{title}</h1>
+            <h1>ID: {id}</h1>
+            <p>Start Datum: {StartDatum}</p>
+            <p>PIN Code: {PINCode}</p>
+            <p>FK Tochten ID: {FKtochtenID}</p>
+            <p>FK Klanten ID: {FKklantenID}</p>
+            <p>FK Statussen ID: {FKstatussenID}</p>
         </section>
-    )
-
+    );
 }
 
-
-
-
-
-
-
-
-
-
-
+export default BoekingenItem;
